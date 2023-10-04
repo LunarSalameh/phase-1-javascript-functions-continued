cfd6db5423ef3ad87ed6d7e77c7c9d04ea636869
LunarSalameh <lun20190983@std.psut.edu.jo>
2023-10-04 14:40:45 +0300

    Completed the Assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // code your solution here
+function saturdayFun(value = "roller-skate") {
+    return `This Saturday, I want to ${value}!`
+}
+
+saturdayFun();
+saturdayFun("bathe my dog");
+
+function mondayWork(value = "go to the office") {
+    return `This Monday, I will ${value}.`;
+}
+
+mondayWork();
+mondayWork("work from home");
+
+
+function wrapAdjective(emphasis) {
+    return function (adjective) {
+        return `You are ${emphasis}${adjective}${emphasis}!`;
+    };
+}
+
+let emphatic = wrapAdjective()("a hard worker");
+
